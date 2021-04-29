24e4105aab91ec88d68149267ff01db1257b8b71
unknown <maor30levy@gmail.com>
2021-04-29 21:39:40 +0300

    ENV patch

--- src/index.js
+++ src/index.js
@@ -2,41 +2,33 @@ const express = require('express');
 const cors = require('cors');
 const hbs = require('hbs');
 const path = require('path');
-const initServer = async () => {
-    try {
-        if (process.env.PORT) {
-            await require('./keys/setKeys').setKeys();
-        }
-        const keys = require('./keys/keys');
-        const port = keys.port;
-        const app = express();
-        const checkURLRouter = require('./router/checkUrlRouter');
-        const publicDirectory = path.join(__dirname, '../public');
-        app.use(express.json());
-        app.use(cors());
-        app.use(express.static(publicDirectory));
-        const viewsPath = path.join(__dirname, "../templates/views");
-        const partialsPath = path.join(__dirname, "../templates/partials");
 
-        app.set('view engine', 'hbs');
-        app.set('views', viewsPath);
-        hbs.registerPartials(partialsPath);
+const port = process.env.PORT;
+const serverHost = process.env.SERVER_HOST;
 
-        app.get('', (req, res) => {
-            res.render('index', {
-                serverHost: keys.serverHost
-            });
-        });
+const app = express();
+const checkURLRouter = require('./router/checkUrlRouter');
+const publicDirectory = path.join(__dirname, '../public');
 
-        app.use(checkURLRouter);
 
-        app.listen(port, () => {
-            console.log(`Server is up on port ${port}!`);
-        });
-    } catch (err) {
-        console.log(err);
-    }
+app.use(express.json());
+app.use(cors());
+app.use(express.static(publicDirectory));
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-};
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
-initServer();
+app.get('', (req, res) => {
+    res.render('index', {
+        serverHost: serverHost
+    });
+});
+
+app.use(checkURLRouter);
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+});
